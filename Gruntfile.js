8635a9b450050c55d477a66a82a2b9fd68f3e198
Harry Mills <harry@haeg.in>
2015-01-27 00:29:34 +0000

    Add Gruntfile.js and package.json
    Ignores node_modules directory because that seems like a sensible idea.

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'styles.css' : 'styles.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: '127.0.0.1'
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask('default',['connect', 'watch']);
+}
